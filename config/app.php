d2c2c4efc4ebd74e8d33bd6adfaf1f477d47eb51
anderson <andersong.salvador@gmail.com>
2023-11-05 00:28:34 -0300

    att app

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://www.fourbiofight.com.br'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
