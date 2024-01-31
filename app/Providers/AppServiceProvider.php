30b1abf3d40cef56dec4a4b3e7129f828ed712b3
anderson654 <andersong.salvador@gmail.com>
2024-01-31 14:55:44 -0300

    att app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if(env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
+            URL::forceRootUrl(Config::get('app.url'));
         }
     }
 }
